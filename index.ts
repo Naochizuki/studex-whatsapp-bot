fc5708dd245291dee71490087e7c244827a6e9c9
Naochizuki <basugandhisasangka@student.uns.ac.id>
2024-11-11 17:06:23 +0700

    Update index.ts

--- index.ts
+++ index.ts
@@ -8,7 +8,7 @@ import "dotenv/config";
 import CommandRoute from "./app/routers/command.route";
 
 export const app = express();
-const port = 8080;
+const port = 3000;
 
 connectDB();
 connectWA();
